1f348e67ce07db74b28eb7a2452abf2da455c8ad
Chakorn Panasutakarn <chakornotaku@gmail.com>
2022-05-08 21:54:35 +0700

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,20 @@
+<?php
+    echo "Connected database server<br>";
+    echo "Selected database";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LEMP STACK DEMO</title>
+</head>
+<body>
+    <h1>LEMP STACK DEMO</h1>
+    <a href="http://0.0.0.0:9200/">CLICK TO PHPMYADMIN</a>
+    <?php phpinfo(); ?>
+</body>
+
+</html>
